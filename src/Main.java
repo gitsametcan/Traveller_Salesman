7e844976c67ff5d833f3097fca4619f56874719f
Can Mayda <canmayda.17@gmail.com>
2023-05-30 19:24:27 +0300

    generatematrix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+class Main {
 
-public class Main {
-
+	public static List<City> cityList = new ArrayList<City>();
 	public static void main(String[] args) {
 		
-
+        
+        
+        City city1 = new City(5,12);
+        City city2 = new City(3,4);
+        City city3 = new City(6,8);
+        City city4 = new City(15,20);
+        City city5 = new City(7,24);
+        cityList.add(city1);
+        cityList.add(city2);
+        cityList.add(city3);
+        cityList.add(city4);
+        cityList.add(city5);
+        
+        double temp = Math.sqrt(Math.pow((city1.getX() - city2.getX()), 2) + Math.pow((city1.getY() - city2.getY()), 2));
+        //System.out.print(temp);
+        
+        TravellerSalesman ts = new TravellerSalesman(cityList);
+        
+        
+        
 	}
 
 }
--- src/TravellerSalesman.java
+++ src/TravellerSalesman.java
@@ -7,8 +7,9 @@ public class TravellerSalesman {
 	private long distance;
 	
 	public TravellerSalesman(List<City> cities) {
-		this.cityList = orderCities(cities);
-		this.distance = calculateDistance(cities);
+		//this.cityList = orderCities(cities);
+		//this.distance = calculateDistance(cities);
+		generateMatrix(cities);
 	}
 	
 	private List<Integer> orderCities(List<City> cities) {
@@ -22,12 +23,19 @@ public class TravellerSalesman {
 	private int[][] generateMatrix(List<City> cities) {
 		//matris burada olusacak
 		
-		int[][] matrixArr = new int[cities.size()][];
+		int[][] matrixArr = new int[cities.size()][cities.size()];
 		
 		for (int i = 0; i < cities.size(); i++) {
 			for(int j = 0; j < cities.size(); j++) {
 				matrixArr[i][j] = (int) twoCityDistance(cities.get(i),cities.get(j));
+				
 			}
+		}
+		for(int i = 0; i < matrixArr.length; i++) {
+			for(int j = 0; j < matrixArr.length; j++) {
+			System.out.print(matrixArr[i][j] + " ");
+			}
+			System.out.println();
 		}
 		
 		
@@ -36,14 +44,16 @@ public class TravellerSalesman {
 	
 	private long calculateDistance(List<City> cities) {
 		long result = 0;
-		for (int i = 0; i<cities.size(); i++) {
+		for (int i = 0; i<cities.size() - 1; i++) {
 			result = result + twoCityDistance(cities.get(i), cities.get(i+1));
 		}
 		return 0;
 	}
 	
 	private long twoCityDistance(City city1, City city2) {
-		return Math.round(Math.sqrt((city1.getX()-city2.getX())^2 + (city1.getY()-city2.getY())^2));
+		long temp = Math.round(Math.sqrt(Math.pow((city1.getX() - city2.getX()), 2) + Math.pow((city1.getY() - city2.getY()), 2)));
+		return temp;
+		
 	}
 
 	public List<Integer> getCityList() {
