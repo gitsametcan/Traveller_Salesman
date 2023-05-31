0a9b9450b8813a1f7a1fa6db5de96f30714cb43c
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2023-05-31 11:21:42 +0300

    Read finished

--- src/ReadAndWrite.java
+++ src/ReadAndWrite.java
@@ -1,11 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ReadAndWrite {
 	
-	private List<City> cities;
+	private static List<City> cities = new ArrayList<City>();
 	
 	public ReadAndWrite(String filename){
 		Scanner scanner;
@@ -14,7 +15,7 @@ public class ReadAndWrite {
 			while (scanner.hasNextLine()) {
 				
 				String line = scanner.nextLine();
-				
+				line = makeNormal(line);
 				addCity(line);
 								
 				 	   	   
@@ -28,23 +29,107 @@ public class ReadAndWrite {
 	}
 	
 	private void addCity(String line) {
-		City city = new City(findId(line),findX(line),findX(line));
-		cities.add(city);
+		City city = new City(findId(line),findX(line),findY(line));
+		this.cities.add(city);
 	}
 	
 	private int findId(String line) {
-		//Burada stringin icindeki 1.sayi int olarak cevrilecek
-		return 0;
+		int s = 0;
+		int t = 0;
+		for(char c : line.toCharArray()) {
+			t ++;
+			if(c==' ') {
+				if(s==0)s = t;
+			}
+		}
+		int Id = 0;
+		String str = line.substring(0, s-1);
+		try{
+            Id = Integer.parseInt(str);
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+        }
+		return Id;
 	}
 	
 	private int findX(String line) {
-		//Burada stringin icindeki 2.sayi int olarak cevrilecek
-		return 0;
+		int s = 0;
+		int f = 0;
+		int t = 0;
+		for(char c : line.toCharArray()) {
+			t ++;
+			if(c==' ') {
+				if(s==0)s = t;
+				else f = t;
+			}
+		}
+		int X = 0;
+		String str = line.substring(s, f-1);
+		try{
+            X = Integer.parseInt(str);
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+        }
+		return X;
 	}
 	
 	private int findY(String line) {
-		//Burada stringin icindeki 3.sayi int olarak cevrilecek
-		return 0;
+		int s = 0;
+		int f = 0;
+		int t = 0;
+		for(char c : line.toCharArray()) {
+			t ++;
+			if(c==' ') {
+				if(s==0)s = 1;
+				else f = t;
+			}
+		}
+		int Y = 0;
+		String str = line.substring(f);
+		try{
+            Y = Integer.parseInt(str);
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+        }
+		return Y;
+	}
+	
+	private String makeNormal(String line) {
+		int t = 0;
+		for(char c : line.toCharArray()) {
+			if(c==' ')t ++;
+			else break;
+		}
+		
+		line = line.substring(t);
+		int s = 0;
+		t = 0;
+		for (char c : line.toCharArray()) {
+			if (s== 0) t++;
+			if (c==' ') {
+				s++;
+			}
+			if (c!=' ' && s>0) break;
+		}
+		line = line.substring(0,t) + line.substring(t+s-1);
+		int k = 0;
+		s = 0;
+		t = 0;
+		for(char c : line.toCharArray()) {
+			if (s == 0) t++;
+			if (c==' ') {
+				if (k==0)k=1;
+				else s++;
+			}
+			if (c!=' ' && s>0) break;
+		}
+		
+		line = line.substring(0,t) + line.substring(t+s-1);
+
+		return line;
 	}
 
 	public List<City> getCities() {
